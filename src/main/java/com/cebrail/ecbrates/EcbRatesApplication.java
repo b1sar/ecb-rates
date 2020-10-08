05f6d76fa6283a73583aa196aaa521e692d50518
b1sar <cebrailylmz02@gmail.com>
2020-10-08 11:30:05 +0300

    Initialize Project

---
+++ src/main/java/com/cebrail/ecbrates/EcbRatesApplication.java
@@ -0,0 +1,13 @@
+package com.cebrail.ecbrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcbRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcbRatesApplication.class, args);
+	}
+
+}
