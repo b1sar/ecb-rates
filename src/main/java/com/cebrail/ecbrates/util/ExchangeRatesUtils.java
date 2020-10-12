14ac89708c0397f01e481eb9b4154bf6f01eff69
b1sar <cebrailylmz02@gmail.com>
2020-10-12 23:02:26 +0300

    unit test and refactor the utility functions

--- src/main/java/com/cebrail/ecbrates/util/ExchangeRatesUtils.java
+++ src/main/java/com/cebrail/ecbrates/util/ExchangeRatesUtils.java
@@ -12,28 +12,69 @@ import java.util.stream.Collectors;
 @Component
 @Scope("singleton")
 public class ExchangeRatesUtils {
-    //utils
-    public void rebase(Day day, Optional<String> b) {
-        List<Currency> currencies = day.getCurrencies();
-        if (b.isPresent() && !b.get().equals("EUR")) {
-            Optional<Currency> base = currencies.stream()
-                    .filter(currency -> b.get().toLowerCase().equals(currency.getName().toLowerCase()))
-                    .findFirst();
-            Double baseRate = base.get().getRate();//TODO: base.get() is used withoed base.isPresent() check. Refactor.
-            for (Currency d : currencies) {
+    public static List<String> supportedCurrencies = List.of("USD", "JPY", "BGN", "CZK", "DKK",
+            "GBP", "HUF", "PLN", "RON", "SEK", "CHF", "ISK", "NOK", "HRK", "RUB", "TRY", "AUD",
+            "BRL", "CAD", "CNY", "HKD", "IDR", "ILS", "INR", "KRW", "MXN", "MYR", "NZD", "PHP",
+            "SGD", "THB", "ZAR");
 
-                d.setRate(d.getRate() / baseRate);
+    public ExchangeRatesUtils() {
+    }
+
+    //utils
+    public void rebase(Day day, Optional<String> offeredBase) {
+
+        List<Currency> currencies = day.getCurrencies();
+        if (offeredBase.isPresent() && !offeredBase.get().equals("EUR")) {
+
+            if (isBaseSupported(offeredBase.get())) {
+                Optional<Currency> base = currencies.stream()
+                        .filter(currency -> offeredBase.get().toLowerCase().equals(currency.getName().toLowerCase()))
+                        .findFirst();
+                /*
+                //TODO: base.get() is used withoed base.isPresent() check. Refactor.
+                If the base is not present, it means that there is no data about the base, in the specific day.
+
+                In case where a time interval is requested to be rebased and although most of the days in the interval
+                have data for that base if a single day happened to have not data, the results would be misleading.
+
+                Because the days that have the 'base' currency data would be rebased and the one's that don't have the 'base'
+                currency data would'nt be rebased, howeser the returned result would be consisted of all of them.
+
+                Possible Solution:
+                throw an exception( for example DayDoesntHaveData(LocalDateTime, base, "there is no data for the day..")
+                and catch that exception in the controller then handle thereafter
+                 */
+                Double baseRate = base.get().getRate();
+                for (Currency d : currencies) {
+
+                    d.setRate(d.getRate() / baseRate);
+                }
+                day.setCurrencies(currencies);
             }
-            day.setCurrencies(currencies);
         }
     }
 
-    public void pickAllSelected(Day day, Optional<List<String>> symbols) {
+    public void pickAllSelected(Day day, Optional<List<String>> symbols) {//filter symbols
         if (symbols.isPresent()) {
-            List<Currency> newCurrencies = day.getCurrencies().stream()
-                    .filter(currency -> symbols.get().contains(currency.getName()))
-                    .collect(Collectors.toList());
-            day.setCurrencies(newCurrencies);
+            if (areAllSymbolsValid(symbols.get())) {
+                List<String> symbolList = symbols.get().stream().map(String::toLowerCase).collect(Collectors.toList());
+
+                List<Currency> newCurrencies = day.getCurrencies().stream()
+                        .filter(currency -> symbolList.contains(currency.getName().toLowerCase()))
+                        .collect(Collectors.toList());
+                day.setCurrencies(newCurrencies);
+            } else {
+                //throw an unsupportedCurrency exception
+            }
         }
+    }
+
+    public boolean isBaseSupported(String base) {
+        return supportedCurrencies.contains(base.toUpperCase());
+    }
+
+    public boolean areAllSymbolsValid(List<String> symbols) {
+        List<String> temp = symbols.stream().map(String::toUpperCase).collect(Collectors.toList());
+        return supportedCurrencies.containsAll(temp);
     }
 }
